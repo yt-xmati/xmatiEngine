c3bc4088e3a2d383cffdfc8ef8c666cc42dda9f3
xmati <93664892+yt-xmati@users.noreply.github.com>
2022-04-01 03:03:22 +0200

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var xmatiEngineVersion:String = '0.1'; //This is also used for Discord RPC
+	public static var xmatiEngineVersion:String = '0.1h'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
